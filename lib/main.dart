6abada9c71c3fade1d502976c741f35f74057230
SMART24\User <a64051700@gmail.com>
2025-03-24 15:59:16 +0900

    세번째 커밋

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mky/drower.dart';
+import 'package:mky/drawer.dart';
+import 'package:mky/setting_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(),
+      initialRoute: "/",
+      routes: {
+        '/': (context) => const MyHomePage(),
+        '/setting': (context) => const SettingPage()
+      },
     );
   }
 }
